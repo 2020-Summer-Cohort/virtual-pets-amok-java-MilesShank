83fc3e947961ba00da907ea8104616d34151cc58
Travis Pettrey <travis.pettrey@gmail.com>
2020-06-03 09:27:20 -0400

    Added grading feedback

--- src/main/java/pets_amok/Application.java
+++ src/main/java/pets_amok/Application.java
@@ -1,4 +1,10 @@
 package pets_amok;
 
+// -Application class needs fleshed out with main method and game loop
+// -The tests you wrote are generally solid, however we need more coverage, particularly in your VirtualPetShelter
+// -PetHealthCalculator is not necessary (although not hurting anything if you wish to keep it)
+// -Since OrganicDog and OrganicCat have identical feed() methods, that behavior could be put in the OrganicPet super class. Look
+//  for instances of duplicate code like this and see if you can structure your program so you only have to write it once
+
 public class Application {
 }
